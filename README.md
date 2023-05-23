04e9ba4cdd3cb87610eeaaf903f7823204b6cf8d
gyoon <xmas6904201@gmail.com>
2023-05-23 11:18:29 +0900

    Oracle Cloud : connection 설정 Test : Cloud connection 테스트

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Boarder
+# 목표 : CRUD 게시판 만들기
+
+# 사용 기술 
+    1. Spring Boot
+    2. Mybatis
+    3. HTML
+    4. CSS
+    5. oracle
---
+++ src/test/java/com/example/boarder/oracle/OracleTest.java
@@ -0,0 +1,30 @@
+package com.example.boarder.oracle;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.sql.SQLException;
+
+@Slf4j
+@SpringBootTest
+public class OracleTest {
+
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Test
+    public void testConnection() throws SQLException {
+        String sql = """
+                select count(*) from test
+                """;
+
+        Integer count = this.jdbcTemplate.queryForObject(sql, Integer.class);
+        log.info("count : {}", count);
+    }
+}
+
+
