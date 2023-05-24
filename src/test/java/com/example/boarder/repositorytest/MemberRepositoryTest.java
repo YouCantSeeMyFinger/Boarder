62213f6ed128446fae728d9207fcde0fa2c38bc9
gyoon <xmas6904201@gmail.com>
2023-05-24 17:43:57 +0900

    SAVE , DELETE , UPDATE , FINDBYID 테스트 코드 작성 및 통과

--- src/test/java/com/example/boarder/repositorytest/MemberRepositoryTest.java
+++ src/test/java/com/example/boarder/repositorytest/MemberRepositoryTest.java
@@ -5,11 +5,9 @@ import com.example.boarder.domain.Member;
 import com.example.boarder.member.dto.MemberDTO;
 import com.example.boarder.member.repository.IMemberRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
