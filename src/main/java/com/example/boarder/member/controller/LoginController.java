d9c3e77afdd2c7da354791d6e90514bd34ad081a
gyoon <xmas6904201@gmail.com>
2023-05-29 19:38:21 +0900

    HTML을 들어갈 시에 LOGIN을 하지도 않았는데 JSESSION이 이미 무작위로 생성되어있다. 자바스크립트로 세션정보를 통해 가져오는 로직으로 인해 생성되는 것으로 판단 하지만 get-session을 해본 결과 아무런 값도 json형태로 표현되지 않은 또한 로그아웃후에는 세션이 만료되게해놔서 현재의 시스템에는 부담이 없다.

--- src/main/java/com/example/boarder/member/controller/LoginController.java
+++ src/main/java/com/example/boarder/member/controller/LoginController.java
@@ -42,14 +42,13 @@ public class LoginController {
 
         Optional<Member> foundMember = this.iMemberService.loginMember(loginForm.getId(), loginForm.getPassword());
 
-        // 서비스 로직에 의해서 회원가입하지 않은 회원 또는 회원이지만 클라이언트가 입력한 정보가 틀렸을 경우 isEmpty
-        if (foundMember.isEmpty()) {
+        if (foundMember.isPresent()) {
+            HttpSession session = request.getSession();
+            session.setAttribute(SessionConst.LOGIN_MEMBER, foundMember.get());
+            return "redirect:/";
+        } else {
             bindingResult.reject("login fail", "아이디 혹은 비밀번호가 맞지 않습니다.");
             return "login/login";
         }
-
-        HttpSession session = request.getSession();
-        session.setAttribute(SessionConst.LOGIN_MEMBER, foundMember.get());
-        return "redirect:/";
     }
 }
