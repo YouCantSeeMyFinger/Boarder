2c0d748d7530781a91fec25d780b3f635b9e7b8f
gyoon <xmas6904201@gmail.com>
2023-06-01 16:43:56 +0900

    Js,HTML 작업 작업 내용 : 동적 page번호 생성 문제 : pagination.appendChild 이유 : prev , Next 안에 pagination을 넣어야한다. 때문에 appendChild를 사용하지말고 직접 해당 범위안에 append를 해줘야한다. 예상 : id로 직접 명시 후에 직접 append하는 방안을 고려

--- src/main/resources/static/js/freeBoarder.js
+++ src/main/resources/static/js/freeBoarder.js
@@ -1,27 +1,61 @@
 $(document).ready(
-    function showPage(pageNum) {
+    () => {
         // 한 페이지에 15개
         const itemPerPage = 15;
+
         // tableRows[n] : n>=0
-        let tableRows = document.querySelectorAll('.table tbody tr');
+        const tableRows = document.querySelectorAll('.table tbody tr');
+
+        // 총 데이터의 갯수
+        const DataNumber = tableRows.length;
+
         // if문에서 사용 할 목적이 있어서 만들어 놓았다.
-        let totalPages = Math.ceil(tableRows / itemPerPage);
+        // 참고로 ceil을 사용하나 이유는 0값을 다루기가 까다롭기 때문이다.
+        const totalPages = Math.ceil(DataNumber / itemPerPage);
+
+        // pagination 선택
+        const pagination = document.querySelector('.pagination');
+
+        // 기존의 pagination은 1개 만들어야할 pagination은 -1개
+        const createPagination = totalPages - 1;
+
 
         // 동작확인 로그
-        // 선택 갯수
-        console.log('tableRows : ', tableRows.length);
-        // 첫번째 요소 text 출력
-        console.log('tableRows first elements :', tableRows[0].textContent);
+        console.log('tableRows :', tableRows.length);
+        console.log('itemPerPage :', itemPerPage);
+        console.log('totalPages :', totalPages);
 
-        let pagination = document.querySelector('.pagination');
+        // TODO 1. 데이터의 개수 만큼 pagination 생성
+        for (let i = 0; i < createPagination; i++) {
+            const pageNumber = i + 2;
+            const listItem = document.createElement('li');
+            const link = document.createElement('a');
+
+            // 클래스 생성
+            listItem.classList.add('page-item');
+            link.classList.add('page-link', 'text-black');
+
+            // 링크 설정
+            link.href = '#' + pageNumber;
+
+            // texcContent 설정 <a>[TextContent]</>
+            link.textContent = pageNumber.toString();
+
+            listItem.appendChild(link);
+            pagination.appendChild(listItem);
+
+            console.log('listItem : ', listItem);
+        }
 
 
-        // pageNavigation 설정
-        if (totalPages === 0) {
-            // 만약 글이 15개 미만 이라면 pagination이 출력 될 필요가 없다.
-            pagination.style.display = 'none';
-        } else {
-            // TODO 이후 로직 처리
+        for (let i = 0; i < tableRows.length; i++) {
+            if (i + 1 <= itemPerPage) {
+                // 15개까지는 출력
+                tableRows[i].style.display = 'table-row';
+            } else {
+                // 15초과는 미출력
+                tableRows[i].style.display = 'none';
+            }
         }
     }
 );
