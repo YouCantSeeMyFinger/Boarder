3f3e89d7361ae79b52c8c361175e2f018b9eb66b
gyoon <xmas6904201@gmail.com>
2023-06-06 15:53:02 +0900

    #1 redirect 사용 (F5시에는 마지막 요청이 무한 반복 동일한 데이터가 여러번 저장 될 수 있는 문제의 소지가 있다.) #2 해당 객체를 redirect의 해당 컨트롤러로 넘기기 위해 RedirectAttribute의 flash를 사용. 일회성이어도 문제가 되지는 않는다.
    findAll하여 FreeBoarder로 Mapping하는 작업시에 변수명으로 인한 오류 때문에 확실히 Mapping되지 않는 문제발생
    
    resultMap을 사용하여 일일이 column과 Field를 Mapping 하는 방법을 선택
    
    추후에 변수명을 지정할 때 DB의 컬럼명이 BOARDER_NUMBER인 경우 필드명은
    boarderNumber로 만들기 (언더바 사용 자제)

--- src/main/java/com/example/boarder/boarder/freeboarder/mapper/IBoarderMapper.java
+++ src/main/java/com/example/boarder/boarder/freeboarder/mapper/IBoarderMapper.java
@@ -6,6 +6,7 @@ import com.example.boarder.member.dto.BoarderDTO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 @Mapper
@@ -17,5 +18,7 @@ public interface IBoarderMapper {
     void updateBoarder(@Param("BoarderDTO") BoarderDTO boarderDTO, @Param("title") String title);
 
     void deleteBoarder(String title);
+
+    List<FreeBoarder> findAllBoarder();
 
 }
--- src/main/java/com/example/boarder/boarder/freeboarder/repository/IBoarderRepo.java
+++ src/main/java/com/example/boarder/boarder/freeboarder/repository/IBoarderRepo.java
@@ -3,6 +3,7 @@ package com.example.boarder.boarder.freeboarder.repository;
 import com.example.boarder.domain.FreeBoarder;
 import com.example.boarder.member.dto.BoarderDTO;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface IBoarderRepo {
@@ -13,4 +14,6 @@ public interface IBoarderRepo {
     public void updateBoarder(BoarderDTO boarderDTO, String title);
 
     public void deleteBoarder(String title);
+
+    public List<FreeBoarder> findAllBoarder();
 }
